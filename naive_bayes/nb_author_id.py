365bc18361a55dd10efe98b0ed14f9f5e45a7379
nodcah <nodcah@gmail.com>
2017-12-21 22:53:57 -0500

    finish mini-project 1

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -9,25 +9,30 @@
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
-sys.path.append("../tools/")
-from email_preprocess import preprocess
 
+from email_preprocess import preprocess
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+from sklearn.naive_bayes import GaussianNB
 
+# Train
+classifier = GaussianNB()
+t0 = time()
+classifier.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
 
+# Predict labels
+t0 = time()
+prediction = classifier.predict(features_test)
+print "training time:", round(time()-t0, 3), "s"
 
-#########################################################
-### your code goes here ###
-
-
-#########################################################
-
-
+# Accuracy
+from sklearn.metrics import accuracy_score
+print accuracy_score(labels_test, prediction)
