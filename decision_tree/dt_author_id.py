9440bf51277624f69f732fa9706b063c433ca5d4
nodcah <nodcah@gmail.com>
2017-12-30 17:36:51 -0500

    Decision trees module done

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -7,25 +7,26 @@
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
+
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+from sklearn import tree
 
+classifier = tree.DecisionTreeClassifier(min_samples_split=40)
 
+classifier.fit(features_train, labels_train)
 
-#########################################################
-### your code goes here ###
+prediction = classifier.predict(features_test)
 
+from sklearn.metrics import accuracy_score
 
-#########################################################
-
-
+print(accuracy_score(labels_test, prediction))
