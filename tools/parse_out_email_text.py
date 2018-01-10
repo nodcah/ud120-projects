e8a969947395dbd4bf93244b07a8ead278c878dc
nodcah <nodcah@gmail.com>
2018-01-10 16:35:30 -0500

    Finishes text_learning module

--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 from nltk.stem.snowball import SnowballStemmer
+from sklearn.feature_extraction import stop_words
+
 import string
 
 def parseOutText(f):
@@ -28,15 +30,14 @@ def parseOutText(f):
         text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
 
         ### project part 2: comment out the line below
-        words = text_string
+        # words = text_string
 
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
-        
-
-
-
+        stemmer = SnowballStemmer("english")
+        for word in text_string.split():
+            words += " " + stemmer.stem(word)
 
     return words
 
