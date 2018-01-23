66a5be7959b31f8a44219d80b74a4d9d873f1cd9
nodcah <nodcah@gmail.com>
2018-01-22 19:47:51 -0500

    Finishes evaluation module

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -13,10 +13,14 @@
 
 import pickle
 import sys
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, recall_score, precision_score
+
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
 ### add more features to features_list!
 features_list = ["poi", "salary"]
@@ -24,8 +28,17 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, random_state=42,
+                                                                            test_size=.3)
 
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
 
-### your code goes here 
+print("accuracy", accuracy_score(labels_test, pred))
+print("recall", recall_score(labels_test, pred))
+print("precision", precision_score(labels_test, pred))
 
-
+print(len([label for label in pred if label == 1]))
+print(len(pred))
+print("true positives:", len([1 for i in range(len(pred)) if pred[i] == labels_test[i] == 1]))
